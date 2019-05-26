d3f1cb6b31c88811eb856c3a9947be99f0ec755d
krystek77 <krystian.wrona77@gmail.com>
2019-05-26 10:33:34 +0200

    Updates

---
+++ precache-manifest.f91dfc692c2c1265ae9a66a71b691499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e71671b384d4c3f235afe6d578cfcb80",
+    "url": "/React-snake-game/static/media/battle.e71671b3.wav"
+  },
+  {
+    "revision": "ec2fd176271cd92421c9",
+    "url": "/React-snake-game/static/js/runtime~main.2f69f12e.js"
+  },
+  {
+    "revision": "4938ebbcf457b5a240bf",
+    "url": "/React-snake-game/static/js/main.4e3e6dac.chunk.js"
+  },
+  {
+    "revision": "a70913b56c951869b06a",
+    "url": "/React-snake-game/static/js/2.85a7381b.chunk.js"
+  },
+  {
+    "revision": "4938ebbcf457b5a240bf",
+    "url": "/React-snake-game/static/css/main.0ed3d4fb.chunk.css"
+  },
+  {
+    "revision": "25578863e550e6994df827abf0f4e740",
+    "url": "/React-snake-game/index.html"
+  }
+];
